9265e84d9e22f5dce29d9468415346a08a06ce3d
tediris <tediris@stanford.edu>
2013-08-16 20:56:17 -0700

    added basic rest api code

--- app.js
+++ app.js
@@ -27,14 +27,55 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.post('/new_user', function (req, res) {
+	if(!req.body.hasOwnProperty('username') || 
+     !req.body.hasOwnProperty('name') || 
+     !req.body.hasOwnProperty('family_username')) {
+    	res.statusCode = 400;
+    	return res.send('Error 400: Post syntax incorrect.');
+  	}
+
+  	var mem = new Member(req.body.name, req.body.username, req.body.family_username);
+  	var currentFamily = lookupUserFamily(mem.username);
+  	currentFamily.members.push(mem);
+  	res.json(true);
+});
+
+app.post('/new_family', function(req, res) {
+	if(!req.body.hasOwnProperty('username') || 
+     !req.body.hasOwnProperty('name')) {
+    	res.statusCode = 400;
+    	return res.send('Error 400: Post syntax incorrect.');
+  	}
+
+  	var fam = new Family(req.body.username, req.body.lastname);
+  	families.push(fam);
+  	res.json(true);
+});
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.get('/family/:family_username', function (req, res) {
+	var family_username = req.params.family_username;
+	res.json(getFamilyByUsername(family_username));
+})
+
+app.get('/user/:username', function (req, res) {
+	var username = req.params.username;
+	var result = {};
+	var family = lookupUserFamily(username);
+	result['user'] = family.getUser(username);
+	result['family'] = family;
+	res.json(result);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
 var families = [];
+var userMap = {};
 
 function Family(username, lastname) {
 	this.username = username;
@@ -53,6 +94,13 @@ function Family(username, lastname) {
 		reward.earned = true;
 		this.currency = this.currency - reward.cost;
 	}
+
+	this.getUser = function(username) {
+		for (int i = 0; i < this.members.length; i++) {
+			if (this.members[i].username == username) return this.members[i];
+		}
+		return null;
+	}
 }
 
 function Member(name, username, family_username) {
@@ -60,6 +108,7 @@ function Member(name, username, family_username) {
 	this.username = username;
 	this.family_username = family_username;
 	this.family = getFamilyByUsername(family_username);
+	userMap[username] = family_username;
 }
 
 function Task(name, description, reward) {
@@ -74,6 +123,10 @@ function Reward(name, description, cost) {
 	this.description = description;
 	this.cost = cost;
 	this.earned = false;
+}
+
+function lookupUserFamily(username) {
+	return userMap[username];
 }
 
 function getFamilyByUsername(family_username) {
